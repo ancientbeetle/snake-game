2076da4720d6630d1f70826bb41291db52bf0b26
AncientBeetle <198234278+ancientbeetle@users.noreply.github.com>
2026-08-16 03:20:28 +0200

    fix: add include guards to apple.h

--- apple.h
+++ apple.h
@@ -1,5 +1,8 @@
 #include "snake.h"
 
+#ifndef S_APPLE
+#define S_APPLE
+
 typedef struct {
     int x;
     int y;
@@ -8,3 +11,5 @@ typedef struct {
 void spawn_apple(applemanager* a_mgr, snakemanager* s_mgr);
 void update_apple(applemanager* a_mgr, snakemanager* s_mgr);
 void draw_apple(applemanager* mgr, SDL_Renderer* renderer);
+
+#endif
