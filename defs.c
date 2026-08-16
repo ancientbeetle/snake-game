b182460cab190bfa4ebddd1c9047aa3b15283323
AncientBeetle <198234278+ancientbeetle@users.noreply.github.com>
2026-08-16 03:17:41 +0200

    feat: use a bigger canvas

--- defs.c
+++ defs.c
@@ -1,10 +1,10 @@
 #include <SDL3/SDL.h>
 
 const int window_width = 640;
-const int window_height = 480;
+const int window_height = 640;
 
 const int canvas_width = 20;
-const int canvas_height = 11;
+const int canvas_height = 20;
 
 const int block_size = 24; // size of every grid block
 const int init_block_x = (window_width - (block_size * canvas_width)) / 2; // X of the first grid block
